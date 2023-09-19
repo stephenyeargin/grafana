5b70858750c1e84f8db6e5cda55a17e1dedab093
suntala <arati.rana@grafana.com>
2023-09-19 15:42:52 +0200

    Add panel titles column as a migration

--- pkg/services/sqlstore/migrations/migrations.go
+++ pkg/services/sqlstore/migrations/migrations.go
@@ -103,6 +103,12 @@ func (*OSSMigrations) AddMigration(mg *Migrator) {
 
 	ualert.MigrationServiceMigration(mg)
 	ualert.CreatedFoldersMigration(mg)
+
+	if mg.Cfg != nil && mg.Cfg.IsFeatureToggleEnabled != nil {
+		if mg.Cfg.IsFeatureToggleEnabled(featuremgmt.FlagPanelTitleSearchInV1) {
+			addDashboardPanelMigrations(mg)
+		}
+	}
 }
 
 func addStarMigrations(mg *Migrator) {
